2f78ccc164f9a4f230a83e2ddd881a3af29d2e50
mahdighadamgahi <mghadamgahi@scmgroup.com>
2025-03-18 12:13:58 +0100

    Update backbones.py

--- src/patchcore/backbones.py
+++ src/patchcore/backbones.py
@@ -3,7 +3,9 @@ import torchvision.models as models  # noqa
 import onnxruntime as ort
 import tensorflow as tf
 import ctypes
-
+import torch
+import qai_hub as hub
+from qai_hub_models.models.wideresnet50_quantized import Model
 _BACKBONES = {
     "alexnet": "models.alexnet(pretrained=True)",
     "bninception": 'pretrainedmodels.__dict__["bninception"](pretrained="imagenet", num_classes=1000)',
@@ -61,8 +63,14 @@ def load_onnx_model(model_path):
     return session
 
 def load_so_model(model_path):
-    model = ctypes.CDLL(model_path)
-    return model
+    torch_model = Model.from_pretrained()
+    device = hub.Device("Samsung Galaxy S23")
+
+# Trace model
+    input_shape = torch_model.get_input_spec()
+    sample_inputs = torch_model.sample_inputs()
+    pt_model = torch.jit.trace(torch_model, [torch.tensor(data[0]) for _, data in sample_inputs.items()])
+    return pt_model
 
 def load(name):
     return eval(_BACKBONES[name])
